f0ec9332624d5b413834b72a9c632efae8e8488a
grupodho <155814123+grupodho@users.noreply.github.com>
2024-06-07 09:12:56 -0100

    Add files via upload

---
+++ Paciente.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Pacote_classes;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author lenovo
+ */
+public class Paciente {
+    String nome;
+    String numDocIdent;
+    LocalDate dataNascimento;
+    String endereco;
+    String telefone;
+    String genero;
+   
+    public boolean CadastrarPaciente(String nome,String numDocIdent,LocalDate dataNascimento,String endereco,String telefone){
+        System.out.println("Nome:");
+        return false;
+        
+        
+       
+        
+    }
+    
+    
+}
